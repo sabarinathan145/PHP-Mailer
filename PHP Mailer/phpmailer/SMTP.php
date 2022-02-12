a8f768639dfb7c732507ce4dfb9fd595d3d92971
K.S.Sabari Nathan <68042285+sabarinathan145@users.noreply.github.com>
2022-02-12 12:51:27 +0530

    final smTp.php

--- PHP Mailer/phpmailer/SMTP.php
+++ PHP Mailer/phpmailer/SMTP.php
@@ -5,12 +5,12 @@
  *
  * @see       https://github.com/PHPMailer/PHPMailer/ The PHPMailer GitHub project
  *
- * @author    Marcus Bointon (Synchro/coolbru) <phpmailer@synchromedia.co.uk>
- * @author    Jim Jagielski (jimjag) <jimjag@gmail.com>
+ * @Marcus Bointon (Synchro/coolbru) <phpmailer@synchromedia.co.uk>
+ * @author    sabarinathan (sabari) <sabari.rishi33@gmail.com>
  * @author    Andy Prevost (codeworxtech) <codeworxtech@users.sourceforge.net>
  * @author    Brent R. Matzelle (original founder)
  * @copyright 2012 - 2019 Marcus Bointon
- * @copyright 2010 - 2012 Jim Jagielski
+ * @copyright 2021 - 2022 sabarinathan
  * @copyright 2004 - 2009 Andy Prevost
  * @license   http://www.gnu.org/copyleft/lesser.html GNU Lesser General Public License
  * @note      This program is distributed in the hope that it will be useful - WITHOUT
